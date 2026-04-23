3cb6d80f50092f17dd76e32ef64b3476c0f999d2
Bilguunhan <psggamer2010@gmail.com>
2026-04-23 19:19:28 +0200

    Update cars.js

--- cars.js
+++ cars.js
@@ -17,25 +17,22 @@ window.addEventListener("scroll", reveal);
 reveal();
 
 const video = document.getElementById("hero-video");
-
 const startTime = 5;
 const endTime = 46;
 
 if (video) {
-
-  // načtení uloženého času
   video.addEventListener("loadedmetadata", () => {
-    const savedTime = localStorage.getItem("videoTime");
-    video.currentTime = savedTime ? savedTime : startTime;
+    video.currentTime = startTime;
+
+    video.play().catch(err => {
+      console.log("Video nejde přehrát:", err);
+    });
   });
 
-  // ukládání času
   video.addEventListener("timeupdate", () => {
-    localStorage.setItem("videoTime", video.currentTime);
-
     if (video.currentTime >= endTime) {
       video.currentTime = startTime;
+      video.play();
     }
   });
-
 }
